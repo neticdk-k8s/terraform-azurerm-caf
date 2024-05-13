c1d046f9a621a972b8b3555f331cf3789af11f93
mfr <mfr@netic.dk>
2024-05-13 11:31:18 +0200

    Remove unnecessary attribute

--- modules/databases/postgresql_flexible_server/managed_identity.tf
+++ modules/databases/postgresql_flexible_server/managed_identity.tf
@@ -2,7 +2,6 @@ module "managed_identity" {
   source   = "../../security/managed_identity"
   for_each = try(var.managed_identity, {})
 
-  resource_id         = azurerm_postgresql_flexible_server.postgresql.id
   name                = each.value.name
   location            = local.location
   resource_group_name = local.resource_group_name
